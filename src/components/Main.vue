86614bf09aaf452209a22c31f23d1bc89844b395
David <dmn-cc@care.coach>
2021-06-03 10:45:52 -0700

    wip

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="6"> David M. Noll </v-col>
+      <v-col cols="6">
+        <a :href="this.paths.resume"> Resume </a>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Main",
+
+  data() {
+    return {
+      paths: {
+        resume: "public/pdf/DavidNoll-Resume.pdf",
+      },
+      links: [
+        // {
+        //   text: "Articles",
+        //   href: "https://medium.com/vuetify",
+        // },
+      ],
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <Main />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Main from "../components/Main.vue";
 
 export default defineComponent({
   name: "Home",
+
   components: {
-    HelloWorld,
+    Main,
   },
 });
 </script>
