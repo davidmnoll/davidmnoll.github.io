465857cc7a02a1ef18ad6ad7136c69ac3f6e004b
davidmnoll <davidmnoll@users.noreply.github.com>
2023-10-20 14:39:38 -0400

    attempting to fix deploy github action issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   // assetsInclude: ["**/*.pdf"],
+  build: {
+    outDir: "_site",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
