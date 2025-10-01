99a9e55276368c819f6299a0afbb018b515289d1
dmn <dmn322@gmail.com>
2025-09-30 21:20:12 -0700

    updated about and resume

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -10,79 +10,79 @@ import {
 
 
 export default function Home() {
-    
+
 
   return (
-    <div   style={{
+    <div style={{
       width: '100%',
       boxSizing: 'border-box',
     }}>
-     {/* <h3 style={{
+      {/* <h3 style={{
         gridColumn: "1/3",
       }}>About</h3> */}
       <div className="grid grid-cols-1 md:grid-cols-2">
 
-      <div style={{
+        <div style={{
           width: '100%',
           maxWidth: '150px',
           margin: '0 auto',
           display: 'block',
           overflow: 'hidden',
-      }}>
-        <img src={profileImage} style={{
-          margin: '0 auto',
-          borderRadius: '15%',
-          width: '150px',
-          height: '150px',
-          objectPosition: '0px 0px',
-          display: 'block',
-          gridColumn: "1",
-          objectFit: 'cover',
-        }} />
-      </div>
-
-      <div className="flex flex-col justify-center">
-
-      <div style={{
-        fontSize: '1.2rem',
-        fontWeight: 'bold',
-        paddingBottom: '1rem',
-        textAlign: 'center',
-      }}>David M Noll</div> 
-
-<div className='grid grid-cols-2'
-        >
-
-        <div style={{
-          padding: '.23rem 0 ',
-          textAlign: 'center',
-          gridColumn: "1",
         }}>
-          <span >  <a className={`text-popover-foreground inline-block`} href="mailto:david.m.noll@gmail.com"> Mail: <EnvelopeClosedIcon className='inline' /></a></span>
-        </div>
-        <div style={{
-          padding: '.23rem 0 ',
-          textAlign: 'center',
-          gridColumn: "2",
-        }}>
-          <span ><a className={`text-popover-foreground inline-block`} href={resumeUrl}> Resume: <FileTextIcon className='inline' /></a></span>
+          <img src={profileImage} style={{
+            margin: '0 auto',
+            borderRadius: '15%',
+            width: '150px',
+            height: '150px',
+            objectPosition: '0px 0px',
+            display: 'block',
+            gridColumn: "1",
+            objectFit: 'cover',
+          }} />
         </div>
 
-        <div style={{
-          padding: '.23rem 0 ',
-          textAlign: 'center',
-          gridColumn: "1",
-        }}>
-          <span > <a className={`text-popover-foreground inline-block`} href="https://github.com/davidmnoll" target="_blank">Github: <GitHubLogoIcon className='inline' /></a></span>
-        </div>
-        <div style={{
-          padding: '.23rem 0 ',
-          textAlign: 'center',
-          gridColumn: "2",
-        }}>
-          <span > <a className={`text-popover-foreground inline-block`} href="https://www.linkedin.com/in/nolldavid/" target='_blank'>LinkedIn: <LinkedInLogoIcon className='inline' /></a></span>
-        </div>
-        </div>
+        <div className="flex flex-col justify-center">
+
+          <div style={{
+            fontSize: '1.2rem',
+            fontWeight: 'bold',
+            paddingBottom: '1rem',
+            textAlign: 'center',
+          }}>David M Noll</div>
+
+          <div className='grid grid-cols-2'
+          >
+
+            <div style={{
+              padding: '.23rem 0 ',
+              textAlign: 'center',
+              gridColumn: "1",
+            }}>
+              <span >  <a className={`text-popover-foreground inline-block`} href="mailto:david.m.noll@gmail.com"> Mail: <EnvelopeClosedIcon className='inline' /></a></span>
+            </div>
+            <div style={{
+              padding: '.23rem 0 ',
+              textAlign: 'center',
+              gridColumn: "2",
+            }}>
+              <span ><a className={`text-popover-foreground inline-block`} href={resumeUrl}> Resume: <FileTextIcon className='inline' /></a></span>
+            </div>
+
+            <div style={{
+              padding: '.23rem 0 ',
+              textAlign: 'center',
+              gridColumn: "1",
+            }}>
+              <span > <a className={`text-popover-foreground inline-block`} href="https://github.com/davidmnoll" target="_blank">Github: <GitHubLogoIcon className='inline' /></a></span>
+            </div>
+            <div style={{
+              padding: '.23rem 0 ',
+              textAlign: 'center',
+              gridColumn: "2",
+            }}>
+              <span > <a className={`text-popover-foreground inline-block`} href="https://www.linkedin.com/in/nolldavid/" target='_blank'>LinkedIn: <LinkedInLogoIcon className='inline' /></a></span>
+            </div>
+          </div>
         </div>
       </div>
       <p style={{
@@ -90,57 +90,58 @@ export default function Home() {
         padding: '1rem 0',
         margin: '1rem 0',
         fontSize: '.8rem',
-      }}  className="border-whispyblue/10 border-b border-t">
-        I am a full stack engineer with experience leading engineering developments in startup environments. 
+      }} className="border-whispyblue/10 border-b border-t">
+        I am a software developer engineer with recent work prototyping and exploring AI applications.  I have full stack and devops experience as well as experience leading engineering developments in startup environments
         <br />
         <br />
-        My experience lies mostly in full stack development for web applications.  I have begun documenting some of my learning of other realms of software development <a href="https://davidmnoll.substack.com/" className='underline'>here</a>.
+        With a background in cognitive science with a focus on linguistics, it's been very interesting to see the developments of LLM's.  I am hoping to apply the theoretical knowledge I've been developing over the years into products which can benefit people.
+        {/* My experience lies mostly in full stack development for web applications.  I have begun documenting some of my learning of other realms of software development <a href="https://davidmnoll.substack.com/" className='underline'>here</a>. */}
         <br />
         {/* <br />
         I'm interested in learning more about all kinds of subjects including workflows, decentralized systems, functional programming, machine learning, combinator & graph rewriting systems
         <br /> */}
         <br />
-        My other academic interests include: information theory, cognitive science, math, physics, history, and statistics.
+        My other academic interests include: cybernetics, complex systems theory, graph rewriting systems, history, category theory.
         <br />
         <br />
-        Outside of work, I enjoy spending time with my wife & son, hiking, playing board games, some video games, and trying to get in shape.
+        Outside of work, I enjoy spending time with my wife & son, hiking, playing board games, and trying to get back in shape.
         {/* <br />
         Favorite things: Incomplete Nature by Terrence Deacon, Clickspring's Antikythera Mechanism video, Tigris & Euphrates by Reiner Knizia, Paths of Glory by Stanley Kubrick, Four Seasons recomposed by Max Richter,  */}
 
       </p>
 
       <div style={{
-          margin: '0 auto',
-          width: '100%'
-        }}>
-      <div style={{
-        gridColumn: "1/3",
-        padding: '1rem 0',
-        margin: '1rem 0',
-        fontSize: '.8rem',
-        // borderTop: '1px solid #333',
-        // borderBottom: '1px solid #333',
+        margin: '0 auto',
+        width: '100%'
       }}>
-        <span style={{
+        <div style={{
           gridColumn: "1/3",
-          fontSize: '1.2rem',
-          fontWeight: 'bold',
-          // paddingBottom: '1rem',
-          display: 'block',
-        }}>Projects</span>
-          {/* <br />
+          padding: '1rem 0',
+          margin: '1rem 0',
+          fontSize: '.8rem',
+          // borderTop: '1px solid #333',
+          // borderBottom: '1px solid #333',
+        }}>
+          <span style={{
+            gridColumn: "1/3",
+            fontSize: '1.2rem',
+            fontWeight: 'bold',
+            // paddingBottom: '1rem',
+            display: 'block',
+          }}>Projects</span>
+          <br />
           <span style={{
             fontSize: '1rem',
-          }}>Fosforescent</span>: (work in progress) a platform & marketplace for collaborative workflows */}
+          }}>Fosforescent:</span> a platform & marketplace for collaborative workflows
           <br />
-          {/* <div style={{
+          <div style={{
             padding: '.5rem 0',
           }}>
             <span style={{
               fontSize: '1rem',
               display: 'inline-block',
-            }}><a href="https://github.com/davidmnoll/netcode">Netcode:</a></span> (work in progress) a neuron-wise neural net library & playground
-          </div> */}
+            }}><a href="https://github.com/davidmnoll/netcode">Netcode:</a></span> a neuron-wise neural net library & playground
+          </div>
           {/* <div style={{
             padding: '.5rem 0',
           }}>
@@ -164,9 +165,9 @@ export default function Home() {
               // fontSize: '1rem',
               display: 'inline-block',
             }} className="underline"><a href="https://github.com/davidmnoll/pixel-time">GLTrader:</a></span> a custom python GUI for trading on the Bittrex exchange
-            </div>
           </div>
         </div>
+      </div>
     </div>
   )
 }
